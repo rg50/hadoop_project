9b9575b7f8b8db55b4eaf4bb8e827679709d6afa
rg50 <gillespieryan@hotmail.com>
2014-02-17 09:02:58 -0600

    Reducer for inverted search

---
+++ reducer_p_search.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+
+oldKey = None
+userIndex = {}
+
+# Loop around the data
+# It will be in the format key\tval\tval2
+# Where key is the post ID, val is the node type and val2 is the post length
+#
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 3:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisWord, thisPost = data_mapped
+
+    # Add word or post id to inverted index for each user
+    if thisKey in userIndex:
+        if thisWord in userIndex[thisKey]:
+            userIndex[thisKey][thisWord].append(thisPost)
+        else:
+            userIndex[thisKey][thisWord] = [thisPost]
+    else:
+        userIndex[thisKey] = {thisWord : [thisPost]}
+
+
+# Print out User Id, word and posts
+for user in userIndex:
+    for word in userIndex[user]:
+        print user, "/t", word, "/t", userIndex[user][word]
+
